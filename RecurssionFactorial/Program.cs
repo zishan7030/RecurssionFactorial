b075c3cd594dd24068c53292b0b12e77cf40d8bf
Dell <Dell@ZISHAN>
2021-09-08 13:54:26 +0530

    Add project files.

---
+++ RecurssionFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RecurssionFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Take User Input Number
+            Console.WriteLine("Enter The Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //Declare Recurssion Function
+            long fact = RecurssionFactorial(num);
+            Console.WriteLine("The Factorial of Number Is: {0}", fact);
+        }
+        //Call Recurssion Function Itself
+        private static long RecurssionFactorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            return num * RecurssionFactorial(num - 1);
+        }
+    }
+}
